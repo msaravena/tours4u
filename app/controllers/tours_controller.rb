4c7f957d5719a143d575116d2fc67b6d8dd4803e
msaravena <aravenap@gmail>
2015-07-11 13:20:21 -0300

    add categories to tours

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -2,6 +2,13 @@ class ToursController < ApplicationController
   before_action :find_tour, only: [:show, :edit, :update, :destroy]
 
   def index
+
+   if params[:category].blank?
+      @tours = Tour.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @tours = Tour.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def new
@@ -25,13 +32,20 @@ class ToursController < ApplicationController
   end
 
   def update
+    if @tour.update(tours_params)
+      redirect_to @tour
+    else
+      render "Edit"
+    end
   end
 
   def destroy
+    @tour.destroy
+    redirect_to root_path
   end
 
   def tours_params
-    params.require(:tour).permit(:tour, :place, :description, :price, :date, :duration, :reservations, :reviews)
+    params.require(:tour).permit(:tour, :place, :description, :price, :date, :duration, :reservations, :reviews, :category_id)
   end
 
   def find_tour
